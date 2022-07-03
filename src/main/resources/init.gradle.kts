12fe8f96a5b47f8f22e1ec72bb0399dc89d9f2cc
johnsonlee <g.johnsonlee@gmail.com>
2022-07-04 03:14:31 +0800

    Refactoring

--- src/main/resources/init.gradle.kts
+++ src/main/resources/init.gradle.kts
@@ -8,7 +8,7 @@ initscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath("io.johnsonlee.tracing:tracing:0.3.0")
+        classpath("io.johnsonlee.tracing:tracing:0.4.0")
     }
 }
 
